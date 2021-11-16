d20ecd775c6eaf9b6cc48589f9c288f8ccb7a118
Jean Yen <yenyichun.developer@gmail.com>
2021-11-16 21:47:48 +0800

    [feature] Show the blur effect by touch seekbar
    Signed-off-by: Jean Yen <yenyichun.developer@gmail.com>

--- app/src/main/java/yichunyen/trial/blureffect/MainActivity.kt
+++ app/src/main/java/yichunyen/trial/blureffect/MainActivity.kt
@@ -1,11 +1,40 @@
 package yichunyen.trial.blureffect
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.RenderEffect
+import android.graphics.Shader
 import android.os.Bundle
+import android.widget.SeekBar
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                applyBlur(p1)
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+                // Do nothing.
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                // Do nothing.
+            }
+
+        })
+    }
+
+    private fun applyBlur(progress: Int) {
+        imageView.setRenderEffect(
+            RenderEffect.createBlurEffect(
+                progress.toFloat(),
+                progress.toFloat(),
+                Shader.TileMode.CLAMP
+            )
+        )
     }
 }
